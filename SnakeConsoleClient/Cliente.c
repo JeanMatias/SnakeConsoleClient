ae3aeda876b2acb87a9b924e639b8aa015e3785c
Jean Matias <jcsmatias84@gmail.com>
2017-04-25 23:49:02 +0100

    Preparar para meter a memoria dinamica e preparar o semaforo da memoria geral dinamico (consoante o numero de utilizadores ligados(falta fazer))

--- SnakeConsoleClient/Cliente.c
+++ SnakeConsoleClient/Cliente.c
@@ -25,12 +25,12 @@ int _tmain(int argc, LPTSTR argv[]) {
 	Msg aux;
 	DWORD tid;
 
-	hMemoria = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, NOMEMEMORIA);
+	hMemoria = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, NOME_MEM_GERAL);
 
 	vistaPartilha = (Msg*)MapViewOfFile(hMemoria, FILE_MAP_ALL_ACCESS, 0, 0, SIZEMENSAGEM);
 
-	hEventoMemoria = CreateEvent(NULL, TRUE, FALSE, EVENTO_MEM);
-	hSemaforoMemoria = CreateSemaphore(NULL, 2, 2, SEMAFORO_MEM);
+	hEventoMemoria = CreateEvent(NULL, TRUE, FALSE, EVNT_MEM_GERAL);
+	hSemaforoMemoria = CreateSemaphore(NULL, 2, 2, SEM_MEM_GERAL);
 	hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Escreve_Memoria, NULL, 0, &tid);
 #ifdef UNICODE
 	_setmode(_fileno(stdin), _O_WTEXT);
@@ -41,12 +41,12 @@ int _tmain(int argc, LPTSTR argv[]) {
 		WaitForSingleObject(hEventoMemoria, INFINITE);
 		WaitForSingleObject(hSemaforoMemoria, INFINITE);
 
-		_tcscpy_s(aux.utilizador, MAXUTILIZADOR, vistaPartilha->utilizador);
+		_tcscpy_s(aux.username, SIZE_USERNAME, vistaPartilha->username);
 		aux.codigoMsg = vistaPartilha->codigoMsg;
 		
 		ReleaseSemaphore(hSemaforoMemoria, 1, NULL);
 
-		_tprintf(TEXT("Nome: %s \tInteiro:%d"), aux.utilizador, aux.codigoMsg);
+		_tprintf(TEXT("Nome: %s \tInteiro:%d"), aux.username, aux.codigoMsg);
 	}
 	
 	return 0;
@@ -57,33 +57,33 @@ int _tmain(int argc, LPTSTR argv[]) {
 /* ---------------------------------------------------------------- */
 DWORD WINAPI Escreve_Memoria(LPVOID param) {
 	Msg aux;
-	TCHAR buf[MAXUTILIZADOR];
+	TCHAR buf[SIZE_USERNAME];
 	_tprintf(TEXT("Nome: "));
 	fflush(stdin);
-	_fgetts(buf, MAXUTILIZADOR, stdin);
+	_fgetts(buf, SIZE_USERNAME, stdin);
 	buf[_tcslen(buf) - 1] = '\0';
-	_tcscpy_s(aux.utilizador, MAXUTILIZADOR,buf);
+	_tcscpy_s(aux.username, SIZE_USERNAME,buf);
 	aux.codigoMsg = CRIARJOGO;
 	//CADA VEZ QUE ESCREVE NA MEM PARTILHADA TEM DE FAZER ISTO
 	for (int i = 0; i < 2; i++) {
 		WaitForSingleObject(hSemaforoMemoria, INFINITE);
 	}
 	vistaPartilha->codigoMsg = aux.codigoMsg;
-	_tcscpy_s(vistaPartilha->utilizador, MAXUTILIZADOR, aux.utilizador);
+	_tcscpy_s(vistaPartilha->username, SIZE_USERNAME, aux.username);
 	SetEvent(hEventoMemoria);
 	ResetEvent(hEventoMemoria);
 	ReleaseSemaphore(hSemaforoMemoria, 2, NULL);
 	while (1) {
 		_tprintf(TEXT("Inteiro: "));
 		fflush(stdin);
-		_fgetts(buf, MAXUTILIZADOR, stdin);
+		_fgetts(buf, SIZE_USERNAME, stdin);
 		buf[_tcslen(buf) - 1] = '\0';
 		aux.codigoMsg=_ttoi(buf);
 		for (int i = 0; i < 2; i++) {
 			WaitForSingleObject(hSemaforoMemoria, INFINITE);
 		}
 		vistaPartilha->codigoMsg = aux.codigoMsg;
-		_tcscpy_s(vistaPartilha->utilizador, MAXUTILIZADOR, aux.utilizador);
+		_tcscpy_s(vistaPartilha->username, SIZE_USERNAME, aux.username);
 		SetEvent(hEventoMemoria);
 		ResetEvent(hEventoMemoria);
 		ReleaseSemaphore(hSemaforoMemoria, 2, NULL);
