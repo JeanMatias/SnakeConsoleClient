fe7501b4ad775cde1cc6ed6ba6b47296603d2427
PAlvesISEC <paulompalves@gmail.com>
2017-04-29 01:43:02 +0100

    Inicio do trabalho de interface no cliente, para continuar....

--- SnakeConsoleClient/Cliente.c
+++ SnakeConsoleClient/Cliente.c
@@ -25,6 +25,8 @@ DWORD WINAPI Interage_Cliente(LPVOID param);
 int _tmain(int argc, LPTSTR argv[]) {
 	MemGeral aux;
 	DWORD tid;
+	TCHAR buffer[TAM_BUFFER];
+	int var_inicio=0;
 
 	hMemoria = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, NOME_MEM_GERAL);
 
@@ -37,25 +39,44 @@ int _tmain(int argc, LPTSTR argv[]) {
 		_tprintf(TEXT("[Erro] Criação de objectos do Windows(%d)\n"), GetLastError());
 		return -1;
 	}
+	
 
-	hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Interage_Cliente, NULL, 0, &tid);
 #ifdef UNICODE
 	_setmode(_fileno(stdin), _O_WTEXT);
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif
 	
-	while (1) {
-		WaitForSingleObject(hEventoMemoria, INFINITE);
-		WaitForSingleObject(hSemaforoMemoria, INFINITE);
+	// Entrada inicial do cliente
+		_tprintf(TEXT("\n\n\t 1 - Ligar servidor local. \n\n\t 2 - Ligar servidor remoto. \n\n\t > "));
+		fflush(stdin);
+		_fgetts(buffer, SIZE_USERNAME, stdin);
+		buffer[_tcslen(buffer) - 1] = '\0';
+		var_inicio = _ttoi(buffer);
 
-		aux.numClientes = vistaPartilha->numClientes;
-		aux.estadoJogo = vistaPartilha->estadoJogo;
 
-		ReleaseSemaphore(hSemaforoMemoria, 1, NULL);
-
-		_tprintf(TEXT("NumClientes: %d \t Estado:%d"), aux.numClientes, aux.estadoJogo);
-	}
 	
+	if (var_inicio == 1) {
+	
+	hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Interage_Cliente, NULL, 0, &tid);
+
+			while (1) {
+				WaitForSingleObject(hEventoMemoria, INFINITE);
+				WaitForSingleObject(hSemaforoMemoria, INFINITE);
+
+				aux.numClientes = vistaPartilha->numClientes;
+				aux.estadoJogo = vistaPartilha->estadoJogo;
+
+				ReleaseSemaphore(hSemaforoMemoria, 1, NULL);
+
+				_tprintf(TEXT("NumClientes: %d \t Estado:%d"), aux.numClientes, aux.estadoJogo);
+			}
+	}
+	else if(var_inicio == 2){
+	
+				_tprintf(TEXT("Lá Lá Lá ....   em manutenção....."));
+				Sleep(1000);
+	}
+		
 	return 0;
 }
 
